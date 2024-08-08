41c5c1b8dde7cf4cbe840be2ed356ca725919858
Egor <egorzozulia@gmail.com>
2024-08-08 11:35:40 +0300

    delete  export

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth';
 import { authConfig } from '../../../../../configs/auth';
 
-export const handler = NextAuth(authConfig);
+const handler = NextAuth(authConfig);
 
 export { handler as GET, handler as POST };
